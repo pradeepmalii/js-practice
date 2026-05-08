74fdf96a8f3a2261bfdd8290979727e73279fb5c
Pradeep Mali <malipradeep591@gmail.com>
2026-05-08 18:55:41 +0530

    implement temporial dead zone with let and const

---
+++ f.let-const/script.js
@@ -0,0 +1,19 @@
+// let a = 10
+// console.log(a)
+let a = 10
+console.log(b)
+var b = 11
+
+// let a = 12 these gives syntax error as variable a is already declare
+// var b = 111  this is valid 
+
+let c
+c = 3 //declare and initialize separately works with let 
+
+// const v
+// v = 9  //these does not works and gives syntax error
+// console.log(v)
+
+
+// const q = 2
+// q = 4 these gives typeerror can not change data afterwards in const
