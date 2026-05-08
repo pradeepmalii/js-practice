783af0e47c904897d0d74e164e7cf936683b1905
Pradeep Mali <malipradeep591@gmail.com>
2026-05-08 18:54:10 +0530

    understand concept of hoisting

---
+++ a.hoisting/script.js
@@ -0,0 +1,26 @@
+// var n = 10;
+
+// function getName(){
+//     console.log("Pradeep")
+// }
+
+// getName()
+// console.log(n)
+
+
+// getResult()
+getMessage()
+console.log(x)
+console.log(a)
+console.log(getMessage)
+
+var x = 10 //here var gives undefined
+let a = 100 //here let gives error 
+
+function getMessage(){
+    console.log("How's going ") //function gets print as whole function code is stored in memory component
+}
+
+var getResult = () => {
+    console.log("Pass")
+}
